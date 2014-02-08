db83e9a336c6dc0e9409c140d9d54e0b773b9b12
Matt Dobson <mdobson4@gmail.com>
2014-02-08 15:25:04 -0500

    Enums 4 all.

--- Siren-Parser/Siren_Action_Field.h
+++ Siren-Parser/Siren_Action_Field.h
@@ -8,12 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, Siren_Field_Type) {
+    Siren_Field_Hidden,
+    Siren_Field_Text,
+    Siren_Field_Search,
+    Siren_Field_Tel,
+    Siren_Field_Url,
+    Siren_Field_Email,
+    Siren_Field_Password,
+    Siren_Field_Datetime,
+    Siren_Field_Date,
+    Siren_Field_Month,
+    Siren_Field_Week,
+    Siren_Field_Time,
+    Siren_Field_Datetime_Local,
+    Siren_Field_Number,
+    Siren_Field_Range,
+    Siren_Field_Color,
+    Siren_Field_Checkbox,
+    Siren_Field_Radio,
+    Siren_Field_File,
+    Siren_Field_Submit,
+    Siren_Field_Image,
+    Siren_Field_Reset,
+    Siren_Field_Button
+};
+
+
 @interface Siren_Action_Field : NSObject
 
 @property NSString *name;
-@property NSString *type;
+@property Siren_Field_Type type;
 @property NSString *value;
 
 -(id) initWithDictionary:(NSDictionary *)data;
+
+-(id) render;
 
 @end
--- Siren-Parser/Siren_Action_Field.m
+++ Siren-Parser/Siren_Action_Field.m
@@ -7,13 +7,84 @@
 //
 
 #import "Siren_Action_Field.h"
+#import <UIKit/UIKit.h>
 
 @implementation Siren_Action_Field
 
 -(id) initWithDictionary:(NSDictionary *)data {
     if (self = [super init]) {
         self.name = data[@"name"];
-        self.type = data[@"type"];
+        if ([data[@"type"] isEqualToString:@"hidden"]) {
+            self.type = Siren_Field_Hidden;
+        }
+        else if ([data[@"type"] isEqualToString:@"hidden"]) {
+            self.type = Siren_Field_Hidden;
+        }
+        else if ([data[@"type"] isEqualToString:@"text"]) {
+            self.type = Siren_Field_Text;
+        }
+        else if ([data[@"type"] isEqualToString:@"search"]) {
+            self.type = Siren_Field_Search;
+        }
+        else if ([data[@"type"] isEqualToString:@"tel"]) {
+            self.type = Siren_Field_Tel;
+        }
+        else if ([data[@"type"] isEqualToString:@"url"]) {
+            self.type = Siren_Field_Url;
+        }
+        else if ([data[@"type"] isEqualToString:@"email"]) {
+            self.type = Siren_Field_Email;
+        }
+        else if ([data[@"type"] isEqualToString:@"password"]) {
+            self.type = Siren_Field_Password;
+        }
+        else if ([data[@"type"] isEqualToString:@"datetime"]) {
+            self.type = Siren_Field_Datetime;
+        }
+        else if ([data[@"type"] isEqualToString:@"date"]) {
+            self.type = Siren_Field_Date;
+        }
+        else if ([data[@"type"] isEqualToString:@"month"]) {
+            self.type = Siren_Field_Month;
+        }
+        else if ([data[@"type"] isEqualToString:@"week"]) {
+            self.type = Siren_Field_Week;
+        }
+        else if ([data[@"type"] isEqualToString:@"time"]) {
+            self.type = Siren_Field_Time;
+        }
+        else if ([data[@"type"] isEqualToString:@"datetime-local"]) {
+            self.type = Siren_Field_Datetime_Local;
+        }
+        else if ([data[@"type"] isEqualToString:@"number"]) {
+            self.type = Siren_Field_Number;
+        }
+        else if ([data[@"type"] isEqualToString:@"range"]) {
+            self.type = Siren_Field_Range;
+        }
+        else if ([data[@"type"] isEqualToString:@"color"]) {
+            self.type = Siren_Field_Color;
+        }
+        else if ([data[@"type"] isEqualToString:@"radio"]) {
+            self.type = Siren_Field_Radio;
+        }
+        else if ([data[@"type"] isEqualToString:@"file"]) {
+            self.type = Siren_Field_File;
+        }
+        else if ([data[@"type"] isEqualToString:@"image"]) {
+            self.type = Siren_Field_Image;
+        }
+        else if ([data[@"type"] isEqualToString:@"reset"]) {
+            self.type = Siren_Field_Reset;
+        }
+        else if ([data[@"type"] isEqualToString:@"button"]) {
+            self.type = Siren_Field_Button;
+        }
+        else if ([data[@"type"] isEqualToString:@"hidden"]) {
+            self.type = Siren_Field_Hidden;
+        }
+        
+        
         if ([data objectForKey:@"value"] != nil) {
             self.value = data[@"value"];
         }
