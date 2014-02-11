95054c394f027a3faf20b70e0968333e4baa6ad7
Matt Dobson <mdobson4@gmail.com>
2014-02-11 09:37:51 -0500

    Added unimplemented tests for the request builder.

---
+++ Siren-ParserTests/Request_BuilderTests.m
@@ -0,0 +1,74 @@
+//
+//  Request_BuilderTests.m
+//  Siren-Parser
+//
+//  Created by Matthew Dobson on 2/11/14.
+//  Copyright (c) 2014 Matthew Dobson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Request_BuilderTests : XCTestCase
+
+@end
+
+@implementation Request_BuilderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testBuildGET
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildPOST
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildPUT
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildDELETE
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildPATCH
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildTRACE
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildOPTIONS
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildCONNECT
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testBuildHEAD
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
